816ed8d446dfd1df351c12e3b02a6281715fb238
Porika Dhanrajnath <dhanrajnath.porika@zemosolabs.com>
2021-07-20 00:58:48 +0530

    spring demo

---
+++ springdemo/src/main/java/com/Spring/springdemo/SpringHelloApp.java
@@ -0,0 +1,22 @@
+package com.Spring.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class SpringHelloApp {
+
+    public static void main(String[] args){
+
+        //load the spring configuration file
+        ClassPathXmlApplicationContext context =
+                new ClassPathXmlApplicationContext("com/Spring.springdemo/applicationContext.xml");
+
+        // retrieve from spring container
+        Coach theCoach = context.getBean("myCoach",Coach.class);
+
+        //call methods on the bean
+        System.out.println(theCoach.getDailyWorkout());
+
+        //close the context
+        context.close();
+    }
+}

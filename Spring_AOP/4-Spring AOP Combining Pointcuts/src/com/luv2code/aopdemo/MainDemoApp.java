039ffbcfd94644d7b23df4fe28010e56fcd696ea
PDhanrajnath <porikadhanrajnath@gmail.com>
2021-07-31 13:47:18 +0530

    Combining @pointcut expressions

---
+++ Spring_AOP/4-Spring AOP Combining Pointcuts/src/com/luv2code/aopdemo/MainDemoApp.java
@@ -0,0 +1,58 @@
+package com.luv2code.aopdemo;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.luv2code.aopdemo.dao.AccountDAO;
+import com.luv2code.aopdemo.dao.MembershipDAO;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class MainDemoApp {
+
+	public static void main(String[] args) {
+		Logger logger=Logger.getLogger(MainDemoApp.class.getName());
+
+		// read spring config java class
+		AnnotationConfigApplicationContext context =
+				new AnnotationConfigApplicationContext(DemoConfig.class);
+		
+		// get the bean from spring container
+		AccountDAO theAccountDAO = context.getBean("accountDAO", AccountDAO.class);
+		
+		// get membership bean from spring container
+		MembershipDAO theMembershipDAO = 
+				context.getBean("membershipDAO", MembershipDAO.class);
+				
+		// call the business method
+		Account myAccount = new Account();
+		theAccountDAO.addAccount(myAccount, true);
+		theAccountDAO.doWork();
+		
+		// call the accountdao getter/setter methods
+		theAccountDAO.setName("foobar");
+		theAccountDAO.setServiceCode("silver");
+
+		String name = theAccountDAO.getName();
+		String code = theAccountDAO.getServiceCode();
+		logger.log(Level.INFO,"Name: "+name);
+		logger.log(Level.INFO,"code: "+code);
+		// call the membership business method
+		theMembershipDAO.addSillyMember();
+		theMembershipDAO.goToSleep();
+		
+		// close the context
+		context.close();
+	}
+
+}
+
+
+
+
+
+
+
+
+
+

07412bc149821b0966cead6fff28e69c8e36f3bc
PDhanrajnath <porikadhanrajnath@gmail.com>
2021-08-02 17:29:34 +0530

    spring-security-demo/basic security

--- Spring_Security/0-Spring Security Demo/src/main/java/com/luv2code/springsecurity/demo/config/MySpringMvcDispatcherServletInitializer.java
+++ Spring_Security/0-Spring Security Demo/src/main/java/com/luv2code/springsecurity/demo/config/MySpringMvcDispatcherServletInitializer.java
@@ -1,12 +1,12 @@
 package com.luv2code.springsecurity.demo.config;
 
+
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 public class MySpringMvcDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
 
 	@Override
 	protected Class<?>[] getRootConfigClasses() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
